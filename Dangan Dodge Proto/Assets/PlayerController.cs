069f4bf60ed9926d8a22c6495fe382cf32e9e85a
John Pham <jopham@amazon.com>
2018-03-19 14:49:51 -0700

    Hope I added the input changs I've made

---
+++ Dangan Dodge Proto/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public int movementSpeed;
+	public string playerNumber;
+
+	private string horizontalAxis;
+	private string verticalAxis;
+
+	// Use this for initialization
+	void Start () {
+		horizontalAxis = playerNumber + "Horizontal";
+		verticalAxis = playerNumber + "Vertical";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis (horizontalAxis) * Time.deltaTime * movementSpeed;
+		var y = Input.GetAxis (verticalAxis) * Time.deltaTime * movementSpeed;
+
+		transform.Translate (x, y, 0);
+	}
+}
