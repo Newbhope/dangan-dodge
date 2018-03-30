cc1055e31ef04c3fef5cda25acc2a0066a80d296
John Pham <jopham@amazon.com>
2018-03-29 20:10:37 -0700

    Doppler at 8

---
+++ dangan-dodge/Assets/Scripts/HitboxController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxController : MonoBehaviour {
+
+	//only really useful if I ever want to change hitbox/modelsize
+
+	public float modelScale;
+	private Transform model;
+
+
+	void Start () {
+		model = GetComponent<Transform>();
+	}
+	
+	void Update () {
+		model.localScale = model.localScale * modelScale;
+	}
+}
