8dd2f43193454fc9d578583c7661869506f94d61
John Pham <jopham@amazon.com>
2018-06-14 22:40:16 -0700

    Finished up movement code

--- dangan-dodge/Assets/Scripts/PlayerController.cs
+++ dangan-dodge/Assets/Scripts/PlayerController.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, yMin, yMax;
+}
+
 public class PlayerController : MonoBehaviour {
 
 	public int movementSpeed;
 	public string playerNumber;
 
-	public float xMin;
-	public float xMax;
-
-	public float yMin;
-	public float yMax;
+	public Boundary bounds;
 
 	private string horizontalAxis;
 	private string verticalAxis;
@@ -31,37 +32,38 @@ public class PlayerController : MonoBehaviour {
 
 		var childComponents = GetComponentsInChildren<Transform>();
 
-
 		foreach (Transform transform in childComponents) {
 			if (transform.tag.Equals("PlayerSprite")) {
 				spriteTransform = transform;
 			}
 		}
 
-		//weird magic number, maybe look into later
-		//works better for .6 scale, approx x = -17.5
-		horizontalLowerLimit = xMin + spriteTransform.localScale.x / 2;
-		horizontalUpperLimit = xMax - spriteTransform.localScale.x / 2;
-
-		verticalLowerLimit = yMin + spriteTransform.localScale.y / 2;
-		verticalUpperLimit = yMax - spriteTransform.localScale.y / 2;
+		horizontalLowerLimit = bounds.xMin + (spriteTransform.localScale.x / 2);
+		horizontalUpperLimit = bounds.xMax - (spriteTransform.localScale.x / 2);
+		verticalLowerLimit = bounds.yMin + (spriteTransform.localScale.y / 2);
+		verticalUpperLimit = bounds.yMax - (spriteTransform.localScale.y / 2);
 	}
-	
+
+	/*
+	 * Player movement applies movement translation then clamps the final position to
+	 * inside the defined limits 
+	 */
 	void Update() {
+		//Translate ranges from -.33 to .33
 		var xTranslate = Input.GetAxis(horizontalAxis) * Time.deltaTime * movementSpeed;
 
 		var yTranslate = Input.GetAxis(verticalAxis) * Time.deltaTime * movementSpeed;
 
-		if (xTranslate + playerTransform.position.x < horizontalLowerLimit 
-			|| xTranslate + playerTransform.position.x > horizontalUpperLimit) {
-			xTranslate = 0;
-		}
-
-		if (yTranslate + playerTransform.position.y < verticalLowerLimit 
-			|| yTranslate + playerTransform.position.y > verticalUpperLimit) {
-			yTranslate = 0;
-		}
-
 		transform.Translate(xTranslate, yTranslate, 0);
+
+		//Why isn't there the weird bouncing effect if I'm applying this after the translation
+		Vector3 clampedPosition = transform.position;
+		clampedPosition.x = Mathf.Clamp(playerTransform.position.x, 
+			horizontalLowerLimit, 
+			horizontalUpperLimit);
+		clampedPosition.y = Mathf.Clamp(playerTransform.position.y, 
+			verticalLowerLimit, 
+			verticalUpperLimit);
+		transform.position = clampedPosition;
 	}
 }
