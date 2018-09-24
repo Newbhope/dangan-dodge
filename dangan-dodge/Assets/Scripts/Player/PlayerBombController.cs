8bf56e56c43a45b6b1bcf4efe55b3840f6d1d349
jpham8 <newbhope8@gmail.com>
2018-09-23 23:51:06 -0700

    Make score UI bigger and add bomb asset

---
+++ dangan-dodge/Assets/Scripts/Player/PlayerBombController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * Class that calls into each bullet spawner with the fire key
+ * Each bullet spawner will have its own configurable fire rate
+ * 
+ * Fire rate is the period in seconds between the next time that type of bullet fires
+ **/
+public class PlayerBombController : MonoBehaviour {
+
+    public Image bombIndicator;
+
+    private string bombButtonName;
+    private int bombsLeft;
+
+    void Start() {
+		BasePlayerVariables vars = this.gameObject.GetComponent<BasePlayerVariables>();
+        bombsLeft = vars.bombsLeft;
+		bombButtonName = vars.playerNumberString + "Bomb";
+	}
+	
+	void Update() {
+		if (Input.GetButton(bombButtonName) && bombsLeft > 0) {
+            GameObject[] bullets = GameObject.FindGameObjectsWithTag("Bullet");
+            foreach (GameObject bullet in bullets) {
+                Destroy(bullet);
+            }
+            Destroy(bombIndicator);
+            bombsLeft--;
+		}
+	}
+}
