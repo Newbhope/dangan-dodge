dbc4a7cdd18769484a51f575fff314f9e370cbbe
Newbhope <newbhope8@gmail.com>
2020-10-29 01:49:35 -0700

    Changing gate movement to lerp. Bigger energy field

--- dangan-dodge/Assets/Scripts/Bullets/GateController.cs
+++ dangan-dodge/Assets/Scripts/Bullets/GateController.cs
@@ -6,17 +6,34 @@ public class GateController : MonoBehaviour
     public int bulletsToFire;
     public float fireRate;
 
+    public Transform start;
+    public Transform end;
+
+    public float speed = 2.0f;
+
+    private float startTime;
+    private float middleTime;
+    private float journeyLength;
+
     private int bulletsFired = 0;
     private BaseBulletSpawner baseBulletSpawner;
 
     private float nextFireTime;
 
+    private bool readyToFire = false;
+    private bool doneFiring = false;
+
     public void Start() {
         baseBulletSpawner = GetComponent<BaseBulletSpawner>();
+
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(start.position, end.position);
     }
 
-    void Update() {
-        if (otherPlayer != null) {
+    void Update()
+    {
+        if (otherPlayer != null)
+        {
             // Face towards enemy
             Vector3 targetPosition = otherPlayer.transform.position;
             Vector3 direction = (targetPosition - transform.position).normalized;
@@ -33,10 +50,51 @@ public class GateController : MonoBehaviour
             // get arc tangent or something
         }
 
+        LerpMove();
 
+        //RawMove();
+    }
+
+    private void LerpMove()
+    {
+        if (transform.position.x < end.position.x)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fractionOfJourney = distCovered / journeyLength;
+            Vector3 test = Vector3.Lerp(start.position, end.position, fractionOfJourney);
+            Vector3 newPosition = new Vector2(test.x, transform.position.y);
+            transform.position = newPosition;
+        }
+
+        if (transform.position.x >= end.position.x)
+        {
+            if (bulletsFired < bulletsToFire && Time.time > nextFireTime)
+            {
+                nextFireTime = Time.time + fireRate;
+                baseBulletSpawner.Spawn();
+                bulletsFired++;
+                middleTime = Time.time;
+            }
+        }
+
+        if (bulletsFired == bulletsToFire)
+        {
+            float distCovered = (Time.time - middleTime) * speed;
+            float fractionOfJourney = distCovered / journeyLength;
+            Vector3 test = Vector3.Lerp(end.position, start.position, fractionOfJourney);
+            Vector2 newPosition = new Vector2(test.x, transform.position.y);
+            transform.position = newPosition;
+
+            // TODO: reset gates
+        }
+
+    }
+
+    private void RawMove()
+    {
         if (transform.localPosition.x <= 2)
         {
-            transform.Translate(0.005f, 0, 0);
+            transform.Translate(0.025f, 0, 0);
         }
 
         if (transform.localPosition.x >= 2)
@@ -52,9 +110,8 @@ public class GateController : MonoBehaviour
 
         if (bulletsFired == bulletsToFire)
         {
+            Debug.Log("BRUH");
             transform.Translate(-0.025f, 0, 0);
         }
     }
-
-
 }
