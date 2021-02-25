91644f5e10b5485f77646dba688d40acf1622da8
Newbhope <newbhope8@gmail.com>
2021-02-24 19:36:47 -0800

    Fix up some code and add needless game summary txt

--- dangan-dodge/Assets/Scripts/GatesOfBabylon/GateController.cs
+++ dangan-dodge/Assets/Scripts/GatesOfBabylon/GateController.cs
@@ -36,25 +36,14 @@ public class GateController : MonoBehaviour
             // Face towards enemy
             Vector3 targetPosition = otherPlayer.transform.position;
             Vector3 direction = (targetPosition - transform.position).normalized;
-            //Debug.Log("direction vector: " + direction);
-            //transform.localRotation.Set(direction.x, direction.y, direction.z, 0);
-            //transform.LookAt(targetPosition, Vector3.forward);
-            //transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
-            //transform.rotation = new Quaternion(0, 0, Quaternion.LookRotation(direction, Vector3.up).x, 0);
-            //transform.eulerAngles = new Vector3(direction.x, direction.y, direction.z);
-            //()
 
             transform.rotation = Quaternion.FromToRotation(Vector3.right, direction);
-            // study trig lol
-            // get arc tangent or something
         }
 
         if (readyToFire == true)
         {
             LerpMove();
         }
-
-        //RawMove();
     }
 
     private void LerpMove()
@@ -105,30 +94,5 @@ public class GateController : MonoBehaviour
         readyToFire = true;
         bulletsFired = 0;
         startTime = Time.time;
-    }
-
-    private void RawMove()
-    {
-        if (transform.localPosition.x <= 2)
-        {
-            transform.Translate(0.025f, 0, 0);
-        }
-
-        if (transform.localPosition.x >= 2)
-        {
-            // shoot
-            if (bulletsFired < bulletsToFire && Time.time > nextFireTime)
-            {
-                nextFireTime = Time.time + fireRate;
-                baseBulletSpawner.Spawn();
-                bulletsFired++;
-            }
-        }
-
-        if (bulletsFired == bulletsToFire)
-        {
-            Debug.Log("BRUH");
-            transform.Translate(-0.025f, 0, 0);
-        }
     }
 }
